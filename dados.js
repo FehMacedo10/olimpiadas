2829b762c465fca2ac1431642c01e39b3a7c4b2a
Felipe Macedo Ribeiro <fmacedoribeiro@outlook.com>
2024-09-11 00:08:59 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos de idade, ela já coleciona títulos e inspira jovens skatistas.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate sk8"
+    },
+
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro na categoria +78kg. Sua força e técnica a tornaram uma das principais atletas do judô mundial.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judoca judo"
+      },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com sua versatilidade e força. Nos Jogos Olímpicos de Tóquio 2020 e Paris 2024, ela conquistou diversas medalhas, tornando-se um dos maiores nomes da ginástica artística brasileira.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginasta ginástica ouro"
+
+    }
+];
+
+
